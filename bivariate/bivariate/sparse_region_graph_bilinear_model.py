42832b22aee972100c2d7ae418dc3a59ecf66fef
Sina Samangooei <sina@yarra.dcs.shef.ac.uk>
2014-02-20 17:41:21 +0000

    trying to get a sparse version working

--- bivariate/bivariate/sparse_region_graph_bilinear_model.py
+++ bivariate/bivariate/sparse_region_graph_bilinear_model.py
@@ -1,6 +1,7 @@
 from pylab import *
 from scipy import sparse as ssp
 import spams
+from IPython.core.debugger import Tracer
 np.random.seed(1)
 # The RGB model, mocked up on synthetic data
 
@@ -8,7 +9,7 @@ R = 3   # regions
 T = 5   # tasks aka number of outputs for each region
 U = 7   # number of users per region, assumed constant and disjoint
 W = 11   # words in vocabulary
-N = 51  # training examples for each region & task
+N = 510  # training examples for each region & task
 
 # the weights we aim to learn
 u = np.random.random((R, T, U))
@@ -42,7 +43,21 @@ Y = np.diagonal(
 
 # add some noise to make things more fun
 Y += np.random.random(Y.shape)
+
 # Y has shape N x T x R
+
+# Let's flatten X and make it sparse
+
+# This is X optimised for U optimisation (i.e. W multiplication)
+Xu = ssp.csr_matrix(X.reshape([N * U * R, W]))
+# Xu now has rows which contain users for each region for each day in that ORDER
+# so the rows of Xu are all the users for a region, then all the regions for a day, then all the days
+
+# This is X optimised for W optimisation (i.e. U multiplication)
+Xw = X.transpose([1,0,3,2]).reshape([R*N*W,U])
+# Xw now has rows which contain words for each day for each region in that ORDER
+# so the rows of Xw are all the words for a day, then all the days for a region
+
 
 # now to learn the fucker
 
@@ -99,33 +114,38 @@ def regulGroups(weights,groups):
 		ind = groups_var[:,g:g+1]
 		tot += max(abs(weights[ind.todense() > 0,:]))
 	return tot
-def regulL1L2(weights):
-	tot = 0
-	# <demo> --- stop ---
-	weights.times(weights)
+# def regulL1L2(weights):
+# 	tot = 0
+# 	# <demo> --- stop ---
+# 	weights.times(weights)
 
-	return tot
+# 	return tot
 for epoch in range(10):
 	# phase 1: learn u & b given fixed w
-	V = np.diagonal(
-		np.tensordot(X,w_hat,axes=([3],[2])),
-		axis1=1, axis2=3
-	)
+	# Here we make V = region stacked (d x u) x t
+	NL = R * U
+	V = [[None] * N] * R
+	for d in range(N):
+		for r in range(R):
+			rowst = r * U + d * NL
+			rowen = (r+1) * U + d * NL
+			# V[r][d] = X[rowst:rowen,:].dot(ssp.csc_matrix(w_hat[r,:,:].T)).T
+			V[r][d] = ssp.csr_matrix(w_hat[r,:,:]).dot(Xu[rowst:rowen,:].T)
+
 	spamsParams['loss'] = "square-missing"
 	for r in range(R):
 		# this is (N x U x T)
-		Vr = V[:,:,:,r]
 		Yr = Y[:,:,r]
-
-		Vrstack = vstack([Vr[:,:,t] for t in range(T)])
+		Vrstack = ssp.vstack(V[r],format="csc")
 		Yrstack = zeros((T*N,T)) + nan
 		for t in range(T):
 			Yrstack[t*N:(t+1)*N,t:t+1] = Yr[:,t:t+1]
 			
 		Yspams = asfortranarray(Yrstack)
-		Xspams = asfortranarray(Vrstack)
+		Xspams = Vrstack
 
 		ur0 = asfortranarray(zeros((Xspams.shape[1],Yspams.shape[1])))
+		# ur0 = ssp.csr_matrix((Xspams.shape[1],Yspams.shape[1]))
 		ur = spams.fistaFlat(
 			Yspams,
 			Xspams,
@@ -134,6 +154,9 @@ for epoch in range(10):
 		)
 
 		u_hat[r,:,:] = ur.T
+
+		# Tracer()()
+
 	# phase 2:
 	# this is: T x N x W x R
 	D = np.diagonal(np.tensordot(u_hat, X, axes=([2],[2])), axis1=3, axis2=0)
@@ -142,16 +165,29 @@ for epoch in range(10):
 	errorBeforeWordOpt = norm(Yest - Y)/2 + regulGroups(flatwhat,groups_var) * graphparams['lambda1']
 	print "(1) User Opt Error: ",errorBeforeWordOpt
 
-	Dstack = zeros((N*R*T,W*R*T))
+	# Dstack = zeros((N*R*T,W*R*T))
+	Dstack = ssp.lil_matrix((N*R*T,W*R*T))
+
 	i = 0;
+	NW = N * W
 	for r in range(R):
+		# I expect this to be (N * W) * U
+		Xwr = Xw[r*NW:(r+1)*NW,:]
 		for t in range(T):
-			Dstack[i*N:(i+1)*N,i*W:(i+1)*W] = D[t,:,:,r]
+			# I expect this to be (N * W) * 1
+			Xwrt = Xwr.dot(u_hat[r,t,:])
+			for n in range(N):
+				DSn = (i * N + n)
+				DSw = (i * W )
+				Dstack.rows[DSn] = range(DSw,DSw + W)
+				Dstack.data[DSn] = Xwrt[n*W:n*W + W]
 			i+=1 
+	
 	Yntrflat = array([ Y.transpose([2,1,0]).flatten()]).T
 	Yspams = asfortranarray(Yntrflat)
-	Xspams = asfortranarray(Dstack)
+	Xspams = Dstack.tocsc()
 	wr0 = asfortranarray(zeros((Xspams.shape[1],Yspams.shape[1])))
+	
 	wr = spams.fistaGraph(
 		Yspams, Xspams, wr0, graph,False,**graphparams
 	)
