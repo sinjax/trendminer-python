3b8a9ddc71501869cf0f0eae17ebe22d40cec50a
Sina Samangooei <sinjax@gmail.com>
2014-05-07 11:24:39 +0100

    fixed the setup

--- bivariate/setup.py
+++ bivariate/setup.py
@@ -8,7 +8,7 @@ setup(
 	author='Sina Samangooei, Bill Lampos, Trevor',
 	author_email='ss@ecs.soton.ac.uk',
 	packages=find_packages(),
-	install_requires=["numpy","scipy","ipython","h5py","git+git://github.com/amueller/word_cloud.git"],
+	install_requires=["numpy","scipy","ipython","h5py"],
 	test_suite="bivariate.test",
 )
 
