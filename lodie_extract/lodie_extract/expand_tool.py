52d241ff9cdf46238eb1051f0c47cc6b8982c0eb
Sina Samangooei <sinjax@gmail.com>
2014-05-07 17:11:51 +0100

    expand tool done

--- lodie_extract/lodie_extract/expand_tool.py
+++ lodie_extract/lodie_extract/expand_tool.py
@@ -1,7 +1,8 @@
 from model import *
 import argparse
-from expand_triples import direct_classes
+from expand_triples import direct_classes, all_classes
 import os
+import logging;logger = logging.getLogger("root")
 
 parser = argparse.ArgumentParser(description='Expand annotations')
 
@@ -9,27 +10,36 @@ subparsers = parser.add_subparsers()
 
 parser_direct = subparsers.add_parser('direct')
 parser_direct.set_defaults(expand=direct_classes)
+parser_all = subparsers.add_parser('all')
+parser_all.set_defaults(expand=all_classes)
 
 def addinout(parser):
 	parser.add_argument('-out', dest="out",type=str,default="out")
 	parser.add_argument('-fmt', dest="rdftype",type=str,default="turtle")
 	parser.add_argument('-in', dest="inp",type=str,required=True,nargs="+")
+	parser.add_argument('-ext', dest="extras",type=str,nargs="+")
 
 addinout(parser_direct)
+addinout(parser_all)
 
 options = parser.parse_args()
-expanded = direct_classes(options.inp,options.out)
+
 if not os.path.exists(options.out): os.makedirs(options.out)
-graph_name = os.path.basename(options.inp[0])
-expanded.serialize(
-	destination=os.sep.join(
-		[
-			options.out,
-			"%s.%s"%(
-				graph_name.split(".")[0],
-				options.rdftype
-			)
-		]
-	),
-	format=options.rdftype
-)
+
+
+for inp in options.inp:
+	graph_name = os.path.basename(inp)
+	logger.debug("Expanding: " + graph_name)
+	expanded = options.expand([inp] + options.extras,options.out)
+	expanded.serialize(
+		destination=os.sep.join(
+			[
+				options.out,
+				"%s.%s"%(
+					graph_name.split(".")[0],
+					options.rdftype
+				)
+			]
+		),
+		format=options.rdftype
+	)
--- lodie_extract/lodie_extract/expand_triples.py
+++ lodie_extract/lodie_extract/expand_triples.py
@@ -6,20 +6,56 @@ DIRECT_CLASSES = """
 	SELECT ?date ?class (SUM(?count) AS ?aggregated)
 	WHERE {
 		?instance tm:word ?word .
-		?instance tm:word ?date .
+		?instance tm:date ?date .
 		?instance tm:count ?count.
 		?word owl:Class ?class
 	} 
 	GROUP BY ?class
 """
-
+format_map = {
+	"owl": "xml"
+}
+def _find_format(inp):
+	ret = inp.split(".")[-1]
+	if ret in format_map:
+		return format_map[ret]
+	return ret;
 def direct_classes(input,output):
 	g = rdflib.Graph()
 	TM = rdflib.Namespace("http://trandminer.org/ontology#")
-	[g.load(file(inp,"r"),format="turtle") for inp in input]
+	[g.load(file(inp,"r"),format=_find_format(inp)) for inp in input]
 	expanded_results = g.query(DIRECT_CLASSES)
 	expanded_graph = rdflib.ConjunctiveGraph()
 	for binding in expanded_results.bindings:
+		discussion = rdflib.BNode()
+		expanded_graph.add((discussion,TM['word'],binding['class']))
+		expanded_graph.add((discussion,TM['date'],binding['date']))
+		expanded_graph.add((discussion,TM['count'],binding['aggregated']))
+	return expanded_graph
+
+ALL_CLASSES = """
+	PREFIX owl: <http://www.w3.org/2002/07/owl#>
+	prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+	SELECT ?date ?class (SUM(?count) AS ?aggregated)
+	WHERE {
+		?instance tm:word ?word .
+		?instance tm:date ?date .
+		?instance tm:count ?count.
+		?word owl:Class/rdfs:subClassOf* ?class
+	} 
+	GROUP BY ?class
+"""
+
+def all_classes(input,output):
+	g = rdflib.Graph()
+	TM = rdflib.Namespace("http://trandminer.org/ontology#")
+	[g.load(file(inp,"r"),format=_find_format(inp)) for inp in input]
+	expanded_results = g.query(ALL_CLASSES)
+	expanded_graph = rdflib.ConjunctiveGraph()
+	for binding in expanded_results.bindings:
+		if not "class" in binding:
+			embed()
+			asdadssd
 		discussion = rdflib.BNode()
 		expanded_graph.add((discussion,TM['word'],binding['class']))
 		expanded_graph.add((discussion,TM['date'],binding['date']))
